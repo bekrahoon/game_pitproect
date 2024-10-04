b2b36669527c6c8308f6d519fcf8c6de858f45b8
Bekrahoon Umurjanov <bekrahoon12@gmail.com>
2024-10-04 21:03:18 +0600

    fix: improved the look shop_game/admin.py shop_game/models.py shop_game/views.py

--- shop_game/models.py
+++ shop_game/models.py
@@ -2,11 +2,14 @@ from django.db import models
 from django.conf import settings
 from django.utils import timezone
 
+
 class Category(models.Model):
     title = models.CharField(max_length=255)
+
     def __str__(self):
         return self.title
-    
+
+
 class About_Game(models.Model):
     title = models.CharField(max_length=255)
     images = models.URLField(blank=False, null=False)
@@ -16,24 +19,27 @@ class About_Game(models.Model):
     download_qty = models.IntegerField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     update_at = models.DateTimeField(default=timezone.now)
+
     def __str__(self):
         return self.title
+
 
 class Cart(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-    
+
     def __str__(self):
         return f"Cart {self.id} for {self.user.username}"
 
+
 class CartItem(models.Model):
-    cart = models.ForeignKey(Cart, related_name='items', on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, related_name="items", on_delete=models.CASCADE)
     game = models.ForeignKey(About_Game, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
-    
+
     def __str__(self):
         return f"{self.quantity} of {self.game.title}"
-    
+
     @property
     def total_price(self):
         return self.quantity * self.game.price
--- shop_game/views.py
+++ shop_game/views.py
@@ -4,11 +4,10 @@ from django.contrib.auth.decorators import login_required
 from .models import About_Game, Cart, CartItem
 from django.utils import timezone
 
-# Create your views here.
 
 def index(request):
     games = About_Game.objects.all()
-    return render(request,'shop_games/games.html',{'games':games})
+    return render(request, "shop_games/games.html", {"games": games})
 
 
 def single_game(request, game_id):
@@ -18,39 +17,36 @@ def single_game(request, game_id):
     #     return render(request, 'shop/single_course.html', {'course':course})
     # except Course.DoesNotExist:
     #     raise Http404()
-    #Option 2
+    # Option 2
     game = get_object_or_404(About_Game, pk=game_id)
-    return render(request, 'shop_games/single_game.html', {'game':game})
-
+    return render(request, "shop_games/single_game.html", {"game": game})
 
 
 @login_required
 def add_to_cart(request, game_id):
     game = get_object_or_404(About_Game, pk=game_id)
-    cart, created = Cart.objects.get_or_create(user=request.user, defaults={'created_at': timezone.now()})
-    
+    cart, created = Cart.objects.get_or_create(
+        user=request.user, defaults={"created_at": timezone.now()}
+    )
+
     cart_item, created = CartItem.objects.get_or_create(cart=cart, game=game)
     if not created:
         cart_item.quantity += 1
         cart_item.save()
-    
-    messages.success(request, 'Game added to cart!')
-    return redirect('shop_game:view_cart')
 
+    messages.success(request, "Game added to cart!")
+    return redirect("shop_game:view_cart")
 
 
 @login_required
 def view_cart(request):
     cart = Cart.objects.filter(user=request.user).first()
-    return render(request, 'shop_games/cart.html', {'cart': cart})
-
+    return render(request, "shop_games/cart.html", {"cart": cart})
 
 
 @login_required
 def remove_from_cart(request, item_id):
     cart_item = get_object_or_404(CartItem, pk=item_id)
     cart_item.delete()
-    messages.success(request, 'Game removed from cart!')
-    return redirect('shop_game:view_cart')
-
-
+    messages.success(request, "Game removed from cart!")
+    return redirect("shop_game:view_cart")
